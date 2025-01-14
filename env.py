f5e51ed7faa987aac0c05011722fd53b06bbd94e
Douglas Mottershead <douglas.mottershead@outlook.com>
2025-01-14 10:20:50 +0000

    add bootsrap template

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"]="postgresql://neondb_owner:w1vsuczkX6Gp@ep-aged-cake-a2pt6pk8.eu-central-1.aws.neon.tech/jolt_radio_vixen_19307"
+
+os.environ["SECRET_KEY"]="b1bdbe953a3d233da3d6083e409f031ffe29fd2fbe28bd238e4b0136180fbfda"
