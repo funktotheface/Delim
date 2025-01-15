866902661f112cf8f951de282577102a76aa7789
Douglas Mottershead <douglas.mottershead@outlook.com>
2025-01-15 11:15:26 +0000

    setup login form

--- inventory/views.py
+++ inventory/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, View
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
 class Index(TemplateView):
     template_name = 'inventory/index.html'
+
+class SignUpView(View):
+    def get(self, request):
+        form = UserRegisterForm()
+        return render(request, 'inventory/signup.html', {'form': form})
+
+    def post(self, request):
+        form = userRegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password1']
+            )
+
+            login(request, user)
+            return redirect('index')
+        
+        return render(request, 'inventory/signup.html', {'form': form})
+            
+
+
