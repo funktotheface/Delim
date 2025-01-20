207ceee3b3caad72c41d7bfbaa85a337684a2331
Douglas Mottershead <douglas.mottershead@outlook.com>
2025-01-20 14:33:17 +0000

    create model from inv items

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class InventoryItem(models.Model):
+    name = models.CharField(max_length=200)
+    quantity = models.IntegerField()
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, blank=True, null=True)
+    expiry_date = models.DateField(null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name    
+    
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
