4bccc8674b6cb4c4096e5cecec6a1bcaffa9e58c
Douglas Mottershead <douglas.mottershead@outlook.com>
2025-01-20 19:12:31 +0000

    add delete function and button

--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, View, CreateView, UpdateView
+from django.views.generic import TemplateView, View, CreateView, UpdateView, DeleteView
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import UserRegisterForm, InventoryItemForm # Add this line to import UserRegisterForm
@@ -59,4 +59,9 @@ class EditItem(LoginRequiredMixin, UpdateView):
     template_name = 'inventory/add_item.html'
     success_url = reverse_lazy('dashboard')
     
+class DeleteItem(LoginRequiredMixin, DeleteView):
+    model = InventoryItem
+    template_name = 'inventory/delete_item.html'
+    success_url = reverse_lazy('dashboard')
+    context_object_name = 'item'
 
