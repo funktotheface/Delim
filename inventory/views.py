5a1046c1c8d552ac6231b24bdbeecfba220fafe4
Douglas Mottershead <douglas.mottershead@outlook.com>
2025-01-21 15:01:10 +0000

    fix sign up template

--- inventory/views.py
+++ inventory/views.py
@@ -23,8 +23,7 @@ class Dashboard(LoginRequiredMixin, View):
         items = InventoryItem.objects.filter(user=self.request.user.id).order_by('id')
         expiring_items = get_expiring_items(self.request.user)
         Expired = date.today()
-        print(Expired)
-        print(Expired)
+
 
         return render(request, 'inventory/dashboard.html', {'items': items, 'expiring_items': expiring_items, 'Expired': Expired})
 
